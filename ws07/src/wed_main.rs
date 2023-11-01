d3edd3d7c6323fbbea25de34a6f802c95b618f8c
imagine-hussain <hussain.nwz000@gmail.com>
2023-11-01 18:03:20 +1100

    o

--- ws07/src/wed_main.rs
+++ ws07/src/wed_main.rs
@@ -13,7 +13,7 @@ enum TaskResult {
 /// A task has "prerequisites" -- it can't run until
 /// they have happened.
 struct Task<'a> {
-    can_run: Box<dyn FnMut(&HashSet<Prerequisites>) -> bool>,
+    prerequisites: HashSet<Prerequisites>,
     task: Box<dyn FnMut() -> TaskResult + 'a>,
 }
 
@@ -32,9 +32,9 @@ impl<'a> Scheduler<'a> {
                 break;
             }
             let random_index = rng.gen_range(0..self.tasks.len());
-            let task = &mut self.tasks[random_index];
-            if (task.can_run)(&self.prerequisites)
-            // .is_subset(&self.prerequisites)
+            if self.tasks[random_index]
+                .prerequisites
+                .is_subset(&self.prerequisites)
             {
                 let mut task = self.tasks.swap_remove(random_index);
                 let task_result = (task.task)();
@@ -101,6 +101,162 @@ fn main() {
     let scheduler = init_scheduler();
     scheduler.start();
 }
+
+// Functoin, Closures, ~lifetimes
+// Scheduler!
+// - Tasks
+// - run those taaask
+// - order
+// - cargo run?
+// - tokio <- ...
+// - cache
+// - yes!!!!! javascrip event loop
+// - "event" -> PQ
+// - async?
+// //
+// await foo();
+// block();
+// // processes -> context switching, threads!
+//
+//
+// Game Engines!
+// - ECS - Entity Component System
+// - uses a scheduler to run functions
+// - 2511 - 2022T2/3
+
+// what is a closure? what is a fucntioon?
+//             ^
+fn foo1() {
+    let v1 = vec![1, 2, 3];
+    let mut sum = String::new();
+    let v2: Vec<_> = v1.into_iter().map(double).collect();
+    // running sum?
+    // captures its environment
+    // ownership, lifetimes, borrowing,
+    // borrow!
+    // i32?
+    // closure, identifgied by the args and return
+    // (i32) -> i32
+    let double = |x: i32| x * 2;
+    let v3: Vec<_> = v2
+        .into_iter()
+        .map(|x| {
+            sum += x.to_string().as_str();
+            // drop takes ownership of sum
+            // drop(sum);
+            x * 2
+        })
+        .collect();
+}
+
+// what is the big limitation here?
+fn double(x: i32) -> i32 {
+    x * 2
+}
+
+//
+// Closure?
+// - its a unique type that cannot be written down
+// -
+//
+// - Fn
+// - FnMut
+// - FnOnce
+//  - can butcher / own things
+//  - only called at most once
+//
+//
+fn takes_ocne(f: impl FnOnce() -> i32) {
+    f();
+    // f(); //
+}
+
+// struct Double {
+//     functioon: *;
+//     sum: &String,
+// }
+// .call
+
+fn takes_mut(mut f: impl FnMut() -> i32) {
+    // how many times?
+    // as many times as want?
+    // how many mutable refs can we have at once?
+    f(); //
+    f(); //
+}
+
+fn takes_fn(f: impl Fn() -> i32) {
+    // how many times?
+    // in total: unlimited
+    // at the same time: unlimited
+    f();
+    f();
+    f();
+    f();
+    f();
+}
+
+// we are a lib autor
+//
+pub fn operatef(f: impl Fn() -> i32) {
+    f();
+    f();
+    f();
+    f();
+    f();
+}
+
+// whty we would use any of the other closure
+//
+// - the writer of the clousre
+// - the user of the closure
+//
+// FnOnce
+//  - drop
+//  - moves so can only call once
+//  - map, filter
+// FnMut
+// - used most places?
+// - reasonable compromise
+// Fn
+// - might want an Fn
+// - concurrency!
+//
+//
+//fn
+//
+fn main2() {
+    let v = vec![1, 2, 3];
+    let mut sum = String::new();
+
+    // FnOnce
+    // FnMut : FnOnce
+    // Fn: FnMut
+    let double = |x| x * 2;
+    let v2 = my_map(v, double);
+    let v2 = my_map_2(v2, double);
+}
+
+pub fn my_map(v: Vec<i32>, f: impl Fn(i32) -> i32) -> Vec<i32> {
+    let mut result = Vec::new();
+
+    for e in v {
+        result.push(f(e));
+    }
+
+    result
+}
+
+pub fn my_map_2(v: Vec<i32>, mut f: impl FnMut(i32) -> i32) -> Vec<i32> {
+    let mut result = Vec::new();
+
+    for e in v {
+        result.push(f(e));
+    }
+
+    result
+}
+
 fn init_scheduler<'a>() -> Scheduler<'a> {
     let mut scheduler = Scheduler::new();
 
@@ -118,9 +274,8 @@ fn init_scheduler<'a>() -> Scheduler<'a> {
     // 2. list of eqns (represent as a directed graph)
     // type inference
     scheduler.add_task(Task {
-        can_run: HashSet::new(),
+        prerequisites: HashSet::new(),
         task: Box::new(move || {
-            // this keeps a ref to time_since_run
             // &String
             println!("last_run, {}", time_since_run_2);
             println!("Cleaning cargo.");
@@ -128,22 +283,11 @@ fn init_scheduler<'a>() -> Scheduler<'a> {
             TaskResult::Finished(HashSet::from([Prerequisites::CleanedCargo]))
         }),
     });
-    // sched, close
-    // sched, close, prereqs
-    // add_task!(sched, || {
-    // ...
-    // })
-    //
-    // add_task!(sched, || {
-    // ...
-    // },
-    // [Prerequisites::CleanedCargo]
-    // )
     dbg!(time_since_run);
 
     // Build the print_randoms binary
     scheduler.add_task(Task {
-        can_run: HashSet::from([Prerequisites::CleanedCargo]),
+        prerequisites: HashSet::from([Prerequisites::CleanedCargo]),
         task: Box::new(|| {
             println!("Building print_randoms.");
             run_command(&["cargo", "build", "--bin", "print_randoms"]);
@@ -153,7 +297,7 @@ fn init_scheduler<'a>() -> Scheduler<'a> {
 
     // Build the program_runner binary
     scheduler.add_task(Task {
-        can_run: HashSet::from([Prerequisites::CleanedCargo]),
+        prerequisites: HashSet::from([Prerequisites::CleanedCargo]),
         task: Box::new(|| {
             println!("Building program_runner");
             run_command(&["cargo", "build", "--bin", "program_runner"]);
@@ -163,7 +307,7 @@ fn init_scheduler<'a>() -> Scheduler<'a> {
 
     // Run the program_runner binary
     scheduler.add_task(Task {
-        can_run: HashSet::from([
+        prerequisites: HashSet::from([
             Prerequisites::CleanedTestFile,
             Prerequisites::CargoBuiltPrintRandoms,
             Prerequisites::CargoBuiltProgramRunner,
@@ -188,7 +332,7 @@ fn init_scheduler<'a>() -> Scheduler<'a> {
     // We also want to make sure there isn't a file called
     // "randoms"
     scheduler.add_task(Task {
-        can_run: HashSet::new(),
+        prerequisites: HashSet::new(),
         task: Box::new(|| {
             println!("Removing randoms.");
             run_command(&["rm", "-f", "randoms"]);
@@ -199,104 +343,3 @@ fn init_scheduler<'a>() -> Scheduler<'a> {
 
     scheduler
 }
-
-fn foo() {
-    let v = vec![1, 2, 3];
-    let mut sum = String::from("");
-    let v2: Vec<_> = v
-        .into_iter()
-        .map(|x| {
-            sum += x.to_string().as_str();
-            // drop(sum);
-            x * 2
-        })
-        .collect();
-}
-
-fn double(x: i32) -> i32 {
-    x * 2
-}
-
-fn uesr() {
-    takes_once(Box::new(|x| x * 2));
-}
-
-fn takes_once(f: Box<dyn Fn(i32) -> i32>) {
-    f(1);
-    // f(1);
-}
-
-// fn foo2<'a>() -> Vec<&'a String> {
-//     let s = String::from("hi");
-//     let v = vec![&s];
-//     v
-// }
-//
-fn cosure_life() -> impl FnMut() {
-    let s = String::from("hi");
-    // let s = s.clone();
-    let z = String::from("hi");
-    let x = {
-        let s = s.clone();
-        move || {
-            println!("{}", s.clone());
-        }
-    };
-    drop(z);
-    dbg!(s);
-    x
-}
-
-fn foo3() {
-    let vec = vec![1, 2, 3];
-    let mut sum = String::from("hif");
-    vec.into_iter()
-        .map(|x| {
-            sum += x.to_string().as_str();
-            x * 2
-        })
-        .collect::<Vec<_>>();
-}
-
-// fn double(x: i32) -> i32 {
-//     x * 2
-// }
-//
-
-// tradeoffs
-//
-// lib authro, client
-//
-// Fn
-// FnMut
-// FnOnce
-
-fn liver() -> impl FnMut(i32) -> i32 {
-    let s = String::from("hi");
-    let c = {
-        let s = s.clone();
-        move |x| {
-            println!("{}", s);
-            x * 2
-        }
-    };
-    dbg!(s);
-
-    return c;
-}
-
-// fn liver_v<'a>() -> Vec<&'a String> {
-//     let s = String::from("hi");
-//     vec![&s]
-// }
-
-// fn do_task<'a>() -> Task<'a> {
-//     let s = String::from("hi");
-//     return Task {
-//         prerequisites: Default::default(),
-//         task: Box::new(|| {
-//             println!("{}", s);
-//             TaskResult::Finished(HashSet::new())
-//         }),
-//     };
-// }
